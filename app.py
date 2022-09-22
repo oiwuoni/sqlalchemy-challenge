2bde95dcfb2bf11769ec653c8274244715ee92e5
Oscar Iwu <nkechiiwu@gmail.com>
2022-09-21 19:49:20 -0500

    Adding the Flask API to the Module 10 repo.

---
+++ app.py
@@ -0,0 +1,78 @@
+
+from tkinter import N
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import json
+import numpy as np
+import datetime as dt
+
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route("/")
+def Homepage():
+    print("Server received request for homepage")
+    return """Homepage<br/> 
+            /api/v1.0/precipitation<br/>
+            /api/v1.0/stations<br/>
+            /api/v1.0/tobs<br/>
+            /api/v1.0/<start><br/>
+            /api/v1.0/<start>/<end>"""
+
+
+@app.route("/api/v1.0/precipitation")
+def Precipitation():
+    print("Server received request for precipitation data")
+    measurement_precipitation = session.query(Measurement.date, Measurement.prcp). \
+    filter(Measurement.date >= '2016-08-23').order_by(Measurement.date.asc()).all()
+    measurement_precipitation_dict = dict(measurement_precipitation)
+
+    return json.dumps(measurement_precipitation_dict, indent = 4)
+
+    
+@app.route("/api/v1.0/stations")
+def Stations():
+    print("Server received request for station data")
+    stations = session.query(Station.station).all()
+    stations_list = list(np.ravel(stations))
+    return jsonify(stations_list)
+
+
+@app.route("/api/v1.0/tobs")
+def Tobs():
+    print("Server received request for temperature observation data")
+    last_year_data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-08-23', \
+                                Measurement.station=='USC00519281').all()
+    last_year_data_list = list(np.ravel(last_year_data))
+    return jsonify(last_year_data_list)
+
+
+@app.route("/api/v1.0/start")
+def Start():
+    print("Server received request for the starting range data")
+    lowest_temp_start = session.query(func.min(Measurement.tobs)).filter(Measurement.date >= '2016-08-23').all()
+    average_temp_start = session.query(func.avg(Measurement.tobs)).filter(Measurement.date >= '2016-08-23').all()
+    highest_temp_start = session.query(func.max(Measurement.tobs)).filter(Measurement.date >= '2016-08-23').all()
+    return jsonify(lowest_temp_start, average_temp_start, highest_temp_start)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
